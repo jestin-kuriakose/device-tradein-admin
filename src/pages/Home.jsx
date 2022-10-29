28d17f7275c1513717296a4eedd37ee6cf82fa13
jestin-kuriakose <jestin.kuriakose@teslaelectronics.ca>
2022-10-29 04:48:58 -0400

    Designed the starting admin dashboard in React and Bootstrap

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import DashboardSummary from '../components/DashboardSummary'
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+import Topbar from '../components/Topbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <div className="container-fluid">
+            <div className="row">
+            <Sidebar/>
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <Topbar/>
+                <canvas className="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+                <DashboardSummary/>
+            </main>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
